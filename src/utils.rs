4b882a000c734e8cedfb2a8f5dc7986890d56ebc
mskrasnov <linuxoid85@gmail.com>
2024-07-03 07:06:17 +0300

    Добавление функции 'utils::read()'

--- src/utils.rs
+++ src/utils.rs
@@ -22,6 +22,24 @@ pub fn read_to_string<P: AsRef<Path>>(path: P) -> Result<String> {
     fs::read_to_string(&path).map_err(|err| Error::new(err).set_kind(ErrorKind::ReadError))
 }
 
+/// Wrapper over [`std::fs::read`]
+///
+/// ## Error kinds
+///
+/// - [`crate::error::ErrorKind::OpenError`] if file does not exist
+/// - [`crate::error::ErrorKind::ReadError`] if an error occured while
+///   reading with `std::fs::read()` function
+pub fn read<P: AsRef<Path>>(path: P) -> Result<Vec<u8>> {
+    if !path.as_ref().exists() {
+        return Err(
+            Error::new(format!("file {} does not exist", &path.as_ref().display()))
+                .set_kind(ErrorKind::OpenError),
+        );
+    }
+
+    fs::read(&path).map_err(|err| Error::new(err).set_kind(ErrorKind::ReadError))
+}
+
 /// Wrapper over [`std::fs::write`]
 ///
 /// ## Error kinds
